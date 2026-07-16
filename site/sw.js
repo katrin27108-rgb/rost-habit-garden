6baf7c7c28f767f87aef66e2b668f6bad1705223
katrin27108-rgb <katrin27108@gmail.com>
2026-07-16 12:52:45 +0300

    Обновить автономную работу

--- site/sw.js
+++ site/sw.js
@@ -1,3 +1,20 @@
+const CACHE = "rost-shell-v2";
+const APP_ROOT = new URL("./", self.location.href).pathname;
+
+self.addEventListener("install", () => self.skipWaiting());
+self.addEventListener("activate", (event) => event.waitUntil(self.clients.claim()));
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET" || new URL(event.request.url).origin !== self.location.origin) return;
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE).then((cache) => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request).then((cached) => cached || caches.match(APP_ROOT))),
+  );
+});
 const CACHE = "rost-github-pages-v1";
 const BASE = new URL("./", self.location.href).pathname;
 
